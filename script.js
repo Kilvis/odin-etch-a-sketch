d3ac5b9ffe19cde024947837db1515daa0b5d7ec
Kilvis <kyndtkilian2006@gmail.com>
2023-06-04 09:23:39 +0200

    added click capability

--- script.js
+++ script.js
@@ -1,27 +1,57 @@
-const container = document.getElementById("container")
+const container = document.getElementById("container");
 
+const btn = document.getElementById("btn");
 
+let size = 16;
+let draw = false;
 
-for (let i = 0; i <= 15; i++) {
-    for (let j = 0; j <= 15; j++) {
-        let box = document.createElement('div');
-        box.className = "container X-" + i + " " +  "Y-" + j;
-        container.appendChild(box)
-        box.style.backgroundColor = 'white'
+adjustSize(size);
+
+btn.addEventListener('click', function() {
+    size = prompt("Enter number between 5 and 100");
+    adjustSize(size);
+})
+
+function adjustSize(size) {
+
+    let grid = container.children;
+    console.log(grid)
+    Array.from(grid).forEach((item) => {
+        container.removeChild(item)
+    })
+        
+    container.style.gridTemplateRows = 'repeat(' + size + ", auto)"
+    container.style.gridTemplateColumns = 'repeat(' + size + ", auto)"
+
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let box = document.createElement('div');
+            box.className = "container X-" + i + " " +  "Y-" + j;
+            container.appendChild(box);
+            box.style.backgroundColor = 'white';
+        }
     }
+
+    grid = container.children;
+
+    Array.from(grid).forEach((item) => {
+        item.addEventListener('mousedown', function() {
+            draw = true;
+        });
+        item.addEventListener('mouseup', function() {
+            draw = false;
+        });
+        item.addEventListener('mouseover', function() {
+            if (draw == true) {
+                item.style.backgroundColor = "grey";
+            }
+           
+        });
+    });
 }
 
-const grid = container.children
 
 
-Array.from(grid).forEach((item) => {
-    item.addEventListener('mouseover', function() {
-        item.style.backgroundColor = "grey"
-    });
-    item.addEventListener('mouseout', function() {
-        item.style.backgroundColor = "white"
-    });
-});
 
 
 
